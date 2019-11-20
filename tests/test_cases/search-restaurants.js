cd897ed26fbd3e3831390fa32dd78ef553941eef
Jacob Torrång <jacob.torrang@skf.com>
2019-11-20 13:09:58 +0100

    Update tests

--- tests/test_cases/search-restaurants.js
+++ tests/test_cases/search-restaurants.js
@@ -4,21 +4,41 @@ const when = require("../steps/when");
 const tearDown = require("../steps/tearDown");
 const given = require("../steps/given");
 
-describe("Given an authenticated user", () => {
-  let user;
+if (process.env.TEST_MODE !== "handler") {
+  describe("Given an authenticated user", () => {
+    let user;
 
-  before(async () => {
-    await init();
-    user = await given.an_authenticated_user();
+    before(async () => {
+      await init();
+      user = await given.an_authenticated_user();
+    });
+
+    after(async () => {
+      await tearDown.an_authenticated_user(user);
+    });
+
+    describe(`When we invoke the POST /restaurants/search endpoint with theme 'cartoon'`, () => {
+      before(async () => await init());
+
+      it(`Should return an array of 4 restaurants`, async () => {
+        let res = await when.we_invoke_search_restaurants(user, "cartoon");
+
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.have.lengthOf(4);
+
+        for (let restaurant of res.body) {
+          expect(restaurant).to.have.property("name");
+          expect(restaurant).to.have.property("image");
+        }
+      });
+    });
   });
-
-  after(() => (await tearDown.an_authenticated_user(user)));
-
+} else {
   describe(`When we invoke the POST /restaurants/search endpoint with theme 'cartoon'`, () => {
     before(async () => await init());
 
     it(`Should return an array of 4 restaurants`, async () => {
-      let res = await when.we_invoke_search_restaurants(user, "cartoon");
+      let res = await when.we_invoke_search_restaurants({}, "cartoon");
 
       expect(res.statusCode).to.equal(200);
       expect(res.body).to.have.lengthOf(4);
@@ -29,4 +49,4 @@ describe("Given an authenticated user", () => {
       }
     });
   });
-});
+}
